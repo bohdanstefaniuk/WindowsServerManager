2f378a5dbe35ced945432f465ed452bdd667a9e0
Bohdan Stefaniuk <bogdanstefanjuk@gmail.com>
2017-12-07 20:18:54 +0200

    #SM-17

---
+++ WindowsServerManager/WebUI.FullFramework/Helpers/JsTreeMenuHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using BLL.Dto;
+using BLL.Services;
+
+namespace WebUI.FullFramework.Helpers
+{
+    public static class JsTreeMenuHelper
+    {
+        public static MvcHtmlString GetJsTreeMenu(this HtmlHelper html)
+        {
+            var rootUl = new TagBuilder("ul");
+            JsTreeModel rootNode;
+            using (var jsTreeMenuGenerator = new JsTreeMenuService())
+            {
+                rootNode = jsTreeMenuGenerator.GetTreeMenuData();
+            }
+
+            GenerateTreeElements(rootNode.Childrens, rootUl, null);
+
+            return new MvcHtmlString(rootUl.ToString());
+        }
+
+        private static void GenerateTreeElements(IEnumerable<JsTreeModel> nodesList, TagBuilder rootUl, TagBuilder previousUl)
+        {
+            previousUl = rootUl;
+            foreach (var node in nodesList)
+            {
+                TagBuilder li = new TagBuilder("li");
+                TagBuilder a = new TagBuilder("a");
+                a.Attributes.Add("href", "#");
+                a.SetInnerText(node.Data);
+                li.InnerHtml += a.ToString();
+                
+
+                if (node.Childrens.Count > 0)
+                {
+                    var ul = new TagBuilder("ul");
+                    GenerateTreeElements(node.Childrens, ul, previousUl);
+                    li.InnerHtml += ul.ToString();
+                    rootUl.InnerHtml += li.ToString();
+                }
+                else
+                {
+                    rootUl.InnerHtml += li.ToString();
+                    rootUl = previousUl;
+                }
+            }
+        }
+    }
+}
--- WindowsServerManager/WebUI.FullFramework/Scripts/Custom/LoadJsTree.js
+++ WindowsServerManager/WebUI.FullFramework/Scripts/Custom/LoadJsTree.js
@@ -1,31 +1,4 @@
 ﻿// Initialize Menu
 function Refresh() {
-    $('#jstree').jstree({
-        'core': {
-            'data': {
-                'url': "/IIS/GetIISMenuModel",
-                'data': function(node) {
-                    return { 'id': node.id };
-                }
-            }
-        }
-        //"json_data": {
-        //    "ajax": {
-        //        "url": "/IISController/GetIISMenuModel",
-        //        "type": "POST",
-        //        "dataType": "json",
-        //        "contentType": "application/json charset=utf-8"
-        //    }
-        //},
-        ////"themes": {
-        ////    "theme": "default",
-        ////    "dots": false,
-        ////    "icons": true,
-        ////    "url": "/content/themes/default/style.css"
-        ////},
-
-        ////"plugins": ["themes", "json_data", "dnd", "contextmenu", "ui", "crrm"]
-        //"plugins": ["json_data"]
-
-    });
+    $('#jstree').jstree();
 }
