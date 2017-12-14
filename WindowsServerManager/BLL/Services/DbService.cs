ff588a72312627a44b5040549cc176f8725bae08
Bohdan Stefaniuk <bogdanstefanjuk@gmail.com>
2017-12-14 22:21:13 +0200

    #SM-23

---
+++ WindowsServerManager/BLL/Services/DbService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using MssqlManager;
+
+namespace BLL.Services
+{
+    public class DbService: IDbService
+    {
+        private readonly DbManager _dbManager;
+
+        public DbService()
+        {
+            //var settingsManager = new SettingsService(new UnitOfWork());
+            //var mssqlSettings = settingsManager.GetSettingByCode("ApplicationDbServer");
+
+            _dbManager = new DbManager(@"WORK-MS-02\MSSQL2016");
+        }
+
+        public async Task DropDatabase(string db)
+        {
+            _dbManager.ConfigureConnectionString(db);
+            await _dbManager.DropDatabaseAsync();
+        }
+        
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        
+    }
+}
