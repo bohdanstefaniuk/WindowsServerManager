91c266fca338a04c1927ac0d6b16dd6a87f12a5c
Bohdan Stefaniuk <bogdanstefanjuk@gmail.com>
2017-12-29 19:35:06 +0200

    Comment thi class for success build

--- WindowsServerManager/WebUI.FullFramework/Core/NLogger.cs
+++ WindowsServerManager/WebUI.FullFramework/Core/NLogger.cs
@@ -1,23 +1,23 @@
-﻿namespace WebUI.FullFramework.Core
-{
-    public class NLogExceptionLogger : ExceptionLogger
-    {
-        private static readonly Logger Nlog = LogManager.GetCurrentClassLogger();
-        public override void Log(ExceptionLoggerContext context)
-        {
-            Nlog.LogException(LogLevel.Error, RequestToString(context.Request), context.Exception);
-        }
+﻿//namespace WebUI.FullFramework.Core
+//{
+//    public class NLogExceptionLogger : ExceptionLogger
+//    {
+//        private static readonly Logger Nlog = LogManager.GetCurrentClassLogger();
+//        public override void Log(ExceptionLoggerContext context)
+//        {
+//            Nlog.LogException(LogLevel.Error, RequestToString(context.Request), context.Exception);
+//        }
 
-        private static string RequestToString(HttpRequestMessage request)
-        {
-            var message = new StringBuilder();
-            if (request.Method != null)
-                message.Append(request.Method);
+//        private static string RequestToString(HttpRequestMessage request)
+//        {
+//            var message = new StringBuilder();
+//            if (request.Method != null)
+//                message.Append(request.Method);
 
-            if (request.RequestUri != null)
-                message.Append(" ").Append(request.RequestUri);
+//            if (request.RequestUri != null)
+//                message.Append(" ").Append(request.RequestUri);
 
-            return message.ToString();
-        }
-    }
-}
+//            return message.ToString();
+//        }
+//    }
+//}
