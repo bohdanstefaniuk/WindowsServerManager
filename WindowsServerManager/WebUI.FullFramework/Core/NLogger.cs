a0b9dfaad4cb42a7aebc1c2245e5116cceef45f0
Bohdan Stefaniuk <bogdanstefanjuk@gmail.com>
2017-12-29 18:44:56 +0200

    Add NLog

---
+++ WindowsServerManager/WebUI.FullFramework/Core/NLogger.cs
@@ -0,0 +1,23 @@
+﻿namespace WebUI.FullFramework.Core
+{
+    public class NLogExceptionLogger : ExceptionLogger
+    {
+        private static readonly Logger Nlog = LogManager.GetCurrentClassLogger();
+        public override void Log(ExceptionLoggerContext context)
+        {
+            Nlog.LogException(LogLevel.Error, RequestToString(context.Request), context.Exception);
+        }
+
+        private static string RequestToString(HttpRequestMessage request)
+        {
+            var message = new StringBuilder();
+            if (request.Method != null)
+                message.Append(request.Method);
+
+            if (request.RequestUri != null)
+                message.Append(" ").Append(request.RequestUri);
+
+            return message.ToString();
+        }
+    }
+}
